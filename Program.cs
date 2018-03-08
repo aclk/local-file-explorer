8f0f1944cde13d46bbac5ed89422b180e16a9abf
Matthew Carney <matthewcarney64@gmail.com>
2018-03-08 11:27:52 +0000

    Updates & polish

--- Program.cs
+++ Program.cs
@@ -5,8 +5,24 @@ namespace RainbowWorm
 {
     class MainClass
     {
+        public const String DEFAULT_TEXT = "-++###########++-";
+        public static ConsoleColor defaultColor = Console.ForegroundColor;
+
         public static void Main(string[] args)
         {
+            string wormTxt = DEFAULT_TEXT;
+
+            // Setup console
+            Console.CursorVisible = false;
+            Console.CancelKeyPress += new ConsoleCancelEventHandler(ExitHandler);
+            
+            // Setup string
+            if (args.Length > 0) {
+                // Use first arg as worm text if its set
+                wormTxt = string.IsNullOrEmpty(args[0]) ? DEFAULT_TEXT : args[0];
+            }
+
+            // Colours to use
             ConsoleColor[] colors = new ConsoleColor[] {
                 ConsoleColor.Blue,
                 ConsoleColor.Cyan,
@@ -18,15 +34,23 @@ namespace RainbowWorm
                 ConsoleColor.Yellow
             };
 
+            // Draw worm
             //Source: https://codegolf.stackexchange.com/a/18733
             int index = 0;
             for (var i = 0d; ;) {
                 if (index == colors.Length) { index = 0; }
                 Console.ForegroundColor = colors[index];
-                Console.Write("{0," + (int)(40 + 20 * Math.Sin(i += .1)) + "}\n", "meow meow meow");
+                Console.Write("{0," + (int)(40 + 20 * Math.Sin(i += .1)) + "}\n", wormTxt);
                 index++;
-                Thread.Sleep(25);//50);
+                Thread.Sleep(25);
             }
+        }
+
+        protected static void ExitHandler(object sender, ConsoleCancelEventArgs args)
+        {
+            // Restore console
+            Console.CursorVisible = true;
+            Console.ForegroundColor = defaultColor;
         }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # RainbowWorm
-Draws a rainbow Sine wave in the console
+Draws a rainbow coloured worm (Sine wave) to the console.
+
+![Look at that worm go!](Properties/worm.gif)
+
+## Download
+
+Download it [here](https://github.com/Killeroo/RainbowWorm/releases)
+
+## Usage
+
+Run without arguments to use default 'worm body' or provide an argument to change text: 
+
+`
+RainbowWorm BlisteringBarnicales 
+`
+`
+RainbowWorm "some text"
+`
+
+Use ctrl-c to stop.
